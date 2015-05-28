ebb8354e59284a5b2058f998c1313df1d1227c6d
Anselm Busse <mail@anselm-busse.de>
2015-05-28 11:18:40 +0200

    Adapt Portux board definitions to v3.8

--- arch/arm/mach-at91/board-portux920t.c
+++ arch/arm/mach-at91/board-portux920t.c
@@ -39,11 +39,11 @@
 #include <asm/mach/map.h>
 #include <asm/mach/irq.h>
 
-#include <mach/board.h>
-#include <mach/at91_aic.h>
 #include <mach/gpio.h>
 #include <mach/cpu.h>
 
+#include "at91_aic.h"
+#include "board.h"
 #include "generic.h"
 #include "clock.h"
 
