6270a2ef7383886845f09bd9bb60c1e95a48bb74
Intel MPSS <mpss@intel.invalid>
2026-09-05 18:30:58 +0200

    drivers: misc MIC driver enablement and backports
    From the Intel MPSS 3.8.6 patched kernel tree (linux-2.6.38+mpss3.8.6),
    decomposed onto vanilla v2.6.38.8 for forward-porting.

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1494,9 +1494,8 @@ static int lo_open(struct block_device *bdev, fmode_t mode)
 	return 0;
 }
 
-static int lo_release(struct gendisk *disk, fmode_t mode)
+static int __lo_release(struct loop_device *lo)
 {
-	struct loop_device *lo = disk->private_data;
 	int err;
 
 	mutex_lock(&lo->lo_ctl_mutex);
@@ -1523,6 +1522,14 @@ static int lo_release(struct gendisk *disk, fmode_t mode)
 out:
 	mutex_unlock(&lo->lo_ctl_mutex);
 out_unlocked:
+	return 0;
+}
+
+static int lo_release(struct gendisk *disk, fmode_t mode)
+{
+	mutex_lock(&loop_devices_mutex);
+	__lo_release(disk->private_data);
+	mutex_unlock(&loop_devices_mutex);
 	return 0;
 }
 
